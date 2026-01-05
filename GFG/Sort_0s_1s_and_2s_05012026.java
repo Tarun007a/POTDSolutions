4384b7628339db9a1aa88c4b132b235b129c973a
Tarun007a <tarunchourey16@gmail.com>
2026-01-05 21:12:23 +0530

    Updated gfg code

--- GFG/Sort_0s_1s_and_2s_05012026.java
+++ GFG/Sort_0s_1s_and_2s_05012026.java
@@ -1,4 +1,27 @@
 package GFG;
 
-public class Sort_0s_1s_and_2s_05012026 {
+// tc - O(n), sc - O(1)
+class Solution {
+    public void sort012(int[] arr) {
+        int lo = 0;
+        int mid = 0;
+        int hi = arr.length-1;
+
+        while(mid <= hi){
+            int ele = arr[mid];
+            if(ele == 0){
+                arr[mid] = arr[lo];
+                arr[lo] = ele;
+                lo++;
+                mid++;
+            }
+            else if(ele == 1){
+                mid++;
+            }
+            else{
+                arr[mid] = arr[hi];
+                arr[hi--] = ele;
+            }
+        }
+    }
 }
