e05bef0a2722bb42e1a740ae1fb13397b3a9f741
Tarun007a <tarunchourey16@gmail.com>
2026-01-09 23:42:37 +0530

    Updated code

--- GFG/Subarrays_with_atmost_k_distinct_integers_09012026.java
+++ GFG/Subarrays_with_atmost_k_distinct_integers_09012026.java
@@ -1,4 +1,24 @@
 package GFG;
 
-public class Subarrays_with_atmost_k_distinct_integers_09012026     {
+// tc - O(n), sc - O(n)
+class Solution {
+    public int countAtMostK(int arr[], int k) {
+        int n = arr.length;
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        int prev = 0;
+        int result = 0;
+
+        for(int i = 0; i < n; i++){
+            mp.put(arr[i], mp.getOrDefault(arr[i], 0)+1);
+
+            while(mp.size() > k){
+                int toRemove = arr[prev++];
+                mp.put(toRemove, mp.get(toRemove)-1);
+                if(mp.get(toRemove) == 0) mp.remove(toRemove);
+            }
+            result += (i-prev+1);
+        }
+        return result;
+    }
 }
+
