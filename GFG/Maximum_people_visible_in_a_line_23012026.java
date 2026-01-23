d5ad951b458572f868d415dda29678750813865d
Tarun007a <tarunchourey16@gmail.com>
2026-01-23 21:27:40 +0530

    Day 133 -> 23/01/2026

--- GFG/Maximum_people_visible_in_a_line_23012026.java
+++ GFG/Maximum_people_visible_in_a_line_23012026.java
@@ -1,4 +1,57 @@
 package GFG;
 
-public class Maximum_people_visible_in_a_line_23012026 {
+// tc - O(n), sc - O(n)
+class Solution {
+    private int[] getNextGreater(int[] arr){
+        int n = arr.length;
+        int[] result = new int[n];
+        Stack<Integer> st = new Stack<>();
+
+        for(int i = 0; i < n; i++){
+            int ele = arr[i];
+
+            while(!st.isEmpty() && arr[st.peek()] <= ele){
+                result[st.pop()] = i;
+            }
+
+            st.push(i);
+        }
+
+        while(!st.isEmpty()){
+            result[st.pop()] = n;
+        }
+        return result;
+    }
+
+    private int[] getPrevGreater(int[] arr){
+        int n = arr.length;
+        int[] result = new int[n];
+        Stack<Integer> st = new Stack<>();
+
+        for(int i = 0; i < n; i++){
+            int ele = arr[i];
+
+            while(!st.isEmpty() && arr[st.peek()] < ele) st.pop();
+
+            if(st.isEmpty()) result[i] = -1;
+            else result[i] = st.peek();
+
+            st.push(i);
+        }
+        return result;
+    }
+
+    public int maxPeople(int[] arr) {
+        int n = arr.length;
+        int result = 0;
+
+        int[] nge = getNextGreater(arr);
+        int[] pge = getPrevGreater(arr);
+
+        for(int i = 0; i < n; i++){
+            int curr = nge[i] - i + i - pge[i] - 1;
+            result = Math.max(curr, result);
+        }
+        return result;
+    }
 }
