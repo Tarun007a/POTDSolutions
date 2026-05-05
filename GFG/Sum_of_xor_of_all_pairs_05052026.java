01d5e7968bf36e742da79d6ab85a45e5361ef932
Tarun007a <tarunchourey16@gmail.com>
2026-05-05 21:10:46 +0530

    Day 235 -> 05/05/2026

---
+++ GFG/Sum_of_xor_of_all_pairs_05052026.java
@@ -0,0 +1,21 @@
+package GFG;
+
+// tc - O(n), sc - O(1)
+class Solution {
+    public long sumXOR(int[] arr) {
+        int n = arr.length;
+        long res = 0;
+        for(int i=0; i<17; i++){
+            long c = 0;
+
+            for(int num : arr){
+                if((num & (1 << i)) != 0){
+                    c++;
+                }
+            }
+            long cf = n - c;
+            res += c * cf *(1L <<i);
+        }
+        return res;
+    }
+}
