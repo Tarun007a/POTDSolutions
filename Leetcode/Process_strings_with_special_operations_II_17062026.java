a90499fbe6c57244073a07f1c8bedea3d1bde998
Tarun007a <tarunchourey16@gmail.com>
2026-06-18 13:30:12 +0530

    Updated solution for 17/06/2026

--- Leetcode/Process_strings_with_special_operations_II_17062026.java
+++ Leetcode/Process_strings_with_special_operations_II_17062026.java
@@ -50,3 +50,42 @@ class Solution {
         return '.';
     }
 }
+
+
+// reverse simulation by me using recursion (learned from code story with mik)
+// tc - O(n), sc - O(n)
+class Solution {
+    private char rec(int i, long k, long len, String s) {
+        char ch = s.charAt(i);
+
+        if(ch == '#') {
+            len = len/2;
+            if(k >= len) k -= len;
+            return rec(i-1, k, len, s);
+        }
+        if(ch == '*') {
+            return rec(i-1, k, len+1, s);
+        }
+        if(ch == '%') {
+            return rec(i-1, len-k-1, len, s);
+        }
+        if(k+1 == len) return ch;
+        return rec(i-1, k, len-1, s);
+    }
+
+    public char processStr(String s, long k) {
+        int n = s.length();
+        long len = 0;
+
+        for(char ch : s.toCharArray()) {
+            if(ch == '*') len--;
+            else if(ch == '#') len *= 2;
+            else if(ch == '%') continue;
+            else len++;
+            if(len < 0) len = 0;
+        }
+
+        if(len <= k) return '.';
+        return rec(n-1, k, len, s);
+    }
+}
