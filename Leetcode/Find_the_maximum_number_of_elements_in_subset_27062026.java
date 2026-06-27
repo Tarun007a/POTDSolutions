a6b391fbb4a2cd6b6af708176f5a53f528f47cf0
Tarun007a <tarunchourey16@gmail.com>
2026-06-27 07:31:08 +0530

    Day 260 -> 27/06/2026

---
+++ Leetcode/Find_the_maximum_number_of_elements_in_subset_27062026.java
@@ -0,0 +1,32 @@
+package Leetcode;
+
+// leetcode - 3020
+// tc - O(nlog(logm)), sc - O(n + m)
+class Solution {
+    private int getLength(long num, HashMap<Integer, Integer> mp) {
+        if(num > 1000_000_000l) return -1;
+
+        int curr = (int)num;
+
+        if(!mp.containsKey(curr)) return -1;
+        if(mp.get(curr) == 1) return 1;
+        return 2 + getLength(num*num, mp);
+    }
+
+    public int maximumLength(int[] nums) {
+        int result = 0;
+        HashMap<Integer, Integer> mp = new HashMap<>();
+
+        for(int num : nums) mp.put(num, mp.getOrDefault(num, 0)+1);
+
+        for(int key : mp.keySet()) {
+            if(key == 1) {
+                int freq = mp.get(1);
+                if((freq&1) == 0) freq--;
+                result = Math.max(result, freq);
+            }
+            else result = Math.max(result, getLength(key, mp));
+        }
+        return result;
+    }
+}
